5287dd0d69a272a7ed7de2211fb1daf838dfd79f
ivesh <iamvenkatesh14@gmail.com>
2024-11-25 12:21:12 +0530

    Tested data_ingestion and prepare base model stage after deleting artifacts locally. The main.py succsessfully executed both stages.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from Kidney_Disease import logger
-logger.info("Logging successul for from Kidney_Disease import logger instead of  from src.Kidney_Disease import logger. The setup.py file is congigured to src not Kidney_Disease.")
 from Kidney_Disease.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+from Kidney_Disease.pipeline.stage2_prepare_base_model import PrepareBasemodelTrainingPipeline
+
 
 STAGE_NAME= "Data ingestion stage"
 
 try:
-    logger.info(f"Data ingestion main funtion has started and it is in stage:{STAGE_NAME}")
+    logger.info(f"Data ingestion main funtion has started and it is in stage:{STAGE_NAME}\n")
     pipeline=DataIngestionTrainingPipeline()
     pipeline.main()
-    logger.info(f"Data ingestion main funtion has completed and it is in stage:{STAGE_NAME}")
+    logger.info(f"Data ingestion main funtion has completed and it is in stage:{STAGE_NAME}\n")
+except Exception as e:
+    logger.exception (e)
+    raise e
+STAGE_NAME= "Prepare Base Model stage"
+
+try:
+    logger.info(f"Prepare base model main funtion has started and it is in stage:{STAGE_NAME}\n")
+    base_model_pipeline=PrepareBasemodelTrainingPipeline()
+    base_model_pipeline.main()
+    logger.info(f"Prepare base model main funtion has completed and it is in stage:{STAGE_NAME}\n")
 except Exception as e:
     logger.exception (e)
     raise e
--- src/Kidney_Disease/pipeline/stage2_prepare_base_model.py
+++ src/Kidney_Disease/pipeline/stage2_prepare_base_model.py
@@ -1,3 +1,28 @@
 from Kidney_Disease.config.configuration import ConfigurationManager
 from Kidney_Disease.components.prepare_base_model import PrepareBaseModel
 from Kidney_Disease import logger
+
+STAGE_NAME="Prepare Base Model"
+
+class PrepareBasemodelTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            prepare_base_model_config = config.get_prepare_base_model_config()
+            prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+            prepare_base_model.get_base_model()
+            prepare_base_model.update_base_model()
+        except Exception as e:
+            raise e
+
+if  __name__=="__main__":
+    try:
+        logger.info(f"*********")
+        logger.info(f"*********{STAGE_NAME} has started********\n")
+        base_model_obj=PrepareBasemodelTrainingPipeline()
+        base_model_obj.main()
+        logger.info(f"*********{STAGE_NAME} has completed******** \n")
+    except Exception as e:
+            raise e        
