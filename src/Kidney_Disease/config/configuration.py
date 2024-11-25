193e1eba06e2db19723fadd8a24076a52ce494fd
ivesh <iamvenkatesh14@gmail.com>
2024-11-25 14:53:40 +0530

    ConfigBox object has no attribute training  was ficed by adding training args in congig.yaml

--- src/Kidney_Disease/config/configuration.py
+++ src/Kidney_Disease/config/configuration.py
@@ -38,7 +38,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "CT-Kidney-Disease-Normal-Cyst-Tumor-Stone")
         create_directories([
             Path(training.root_dir)
         ])
