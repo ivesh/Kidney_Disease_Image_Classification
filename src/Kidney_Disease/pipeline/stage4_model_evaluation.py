4d0f951fbcd0eeebbda50d44413d3e8e7b296df0
ivesh <iamvenkatesh14@gmail.com>
2024-11-27 11:19:03 +0530

    Error detected in model evaluation in the main file hence updated the workflow hashed out pipeline model evaluation code

--- src/Kidney_Disease/pipeline/stage4_model_evaluation.py
+++ src/Kidney_Disease/pipeline/stage4_model_evaluation.py
@@ -5,14 +5,15 @@ from Kidney_Disease import logger
 STAGE_NAME="Model Evaluation"
 
 class EvaluationPipeline:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
     def main(self):
         config = ConfigurationManager()
         eval_config = config.get_evaluation_config()
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
-        evaluation.log_into_mlflow()
+        evaluation.save_score()
+        #evaluation.log_into_mlflow()
 
 if __name__=="__main__":
     try:
